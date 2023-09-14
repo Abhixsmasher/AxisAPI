1bffd3ae811a11b38affb28ea6ccea62f5ffe65a
Abhixsmasher <71562641+Abhixsmasher@users.noreply.github.com>
2023-09-14 15:03:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents import create_csv_agent
+import pandas
 
 app = Flask(__name__)
 
