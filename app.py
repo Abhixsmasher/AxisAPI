4237253d1e312d65dec653c773e95ad52a0954ee
Abhixsmasher <71562641+Abhixsmasher@users.noreply.github.com>
2023-12-28 12:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ from email.mime.application import MIMEApplication
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
-from langchain.agents import create_csv_agent
 import pandas as pd
 import hashlib
 
